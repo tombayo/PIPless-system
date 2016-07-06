3e2647d9543efbd8043077b8a3cdbda8c7f6c656
tombayo <me@tombayo.com>
2016-07-06 17:54:11 +0200

    Multiple updates towards version 1.1.0 with PHP7 support.

--- pl.php
+++ pl.php
@@ -1,30 +1,31 @@
-<?php
+<?php declare(strict_types=1);
 /**
-* PIPless v1.0.2
-* 
-* @package  PIPless
-* @version  1.0.2
-* @desc     PIPless is a tiny application framework forked from Gilbert Pellegrom's PIP. PIPless is modified to use GET-variables and static objects for an even simpler framework. It is also designed to work directly with RedBeanPHP (redbeanphp.com)
-* @author   @tombayo <me@tombayo.com>
-* @license  GPLv3
-* @link     https://github.com/tombayo/PIPless
-*/
+ * PIPless
+ * 
+ * This is the main class of the system, and handles the initial execution
+ * of the framework.
+ * 
+ * @package pipless
+ * 
+ * @link https://github.com/tombayo/PIPless
+ * @author  Gilbert Pellegrom, Dev7studios
+ * @author  Tom Andre Munkhaug, @tombayo <me@tombayo.com>
+ * @license GPLv3
+ */
 class PIPless {
   
   /**
   * Called to execute the framework.
   * 
-  * Will load a controller based on the $_GET['p']-variable.
-  * If url rewriting is turned on in the config-file, a controller is loaded based on the url instead.
+  * Will load a controller based on the $_GET['p']-variable, if url_rewrite
+  * is disabled, otherwise it loads a controller based on the requested uri.
   * 
   * @author  Gilbert Pellegrom, Dev7studios
-  * @license MIT
-  * @see     PIP.license.txt
+  * @author  Tom Andre Munkhaug, @tombayo <me@tombayo.com>
+  * @license GPLv3
   * 
-  * @param void
-  * @returns void
-  * @static
-  * @access public
+  * @global $config Used to save some dynamic global variables from DB.
+  * 
   */
   static public function go() {
 	  global $config;
@@ -33,26 +34,27 @@ class PIPless {
     $controller = $config['default_controller'];
     $action = 'index';
     $argument = '';
-    
+
     if ($config['url_rewrite']) {
       // Get request url and script url
-      $request_url = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
-      $script_url  = (isset($_SERVER['PHP_SELF'])) ? $_SERVER['PHP_SELF'] : '';
+      $request_url = $_SERVER['REQUEST_URI'] ?? '';
+      $script_url  = $_SERVER['PHP_SELF'] ?? '';
        
       // Get our url path and trim the / of the left and the right
       if($request_url != $script_url) $url = trim(preg_replace('/'. str_replace('/', '\/', str_replace('index.php', '', $script_url)) .'/', '', $request_url, 1), '/');
     } else {
-      $url = isset($_GET['p']) ? $_GET['p'] : '';
+      $url = $_GET['p'] ?? '';
     }
     
 	  // Split the url into segments
 	  $segments = explode('/', $url);
 	  
 	  // Do our default checks
+	  // @todo Could the be replaced by the null coalescing operator?
 	  if(isset($segments[0]) && $segments[0] != '') $controller = $segments[0];
 	  if(isset($segments[1]) && $segments[1] != '') $action = $segments[1];
-	  if(isset($segments[1]) && $segments[1] != '') $argument = $segments[2];
-
+	  if(isset($segments[2]) && $segments[2] != '') $argument = $segments[2];
+    
 	  // Get our controller file
     $path = APP_DIR . 'controllers/' . $controller . '.php';
 	  if(file_exists($path)) {
@@ -66,80 +68,157 @@ class PIPless {
     if(!is_callable([$controller, $action])) {
       $action = 'index';
     }
+    // Runs the selected method from the selected controller, then exits.
     die($controller::$action($argument));
   }
 }
 
 /**
+ * Model
+ * 
+ * 
+ * Used for requests to the database.
+ * 
+ * @author Tom Andre Munkhaug <me@tombayo.com>
+ * @package pipless
+ * @subpackage system
+ * 
+ */
+class Model {
+  
+  /**
+  * Initialize RedBeanPHP ORM for mysql.
+  * 
+  * Initializes RedBeanPHP and returns the Toolbox, for more details:
+  * @link http://redbeanphp.com/api/class-RedBeanPHP.ToolBox.html
+  * 
+  * @global $config Used to get database-settings
+  * 
+  * @return RedBeanPHP\ToolBox
+  */
+  public static function initDB():RedBeanPHP\ToolBox {
+    global $config;
+    if (!class_exists('R')) {
+      Load::plugin("rb"); // Loads RedBeanPHP, our ORM
+      R::setup('mysql:host='.$config['db_host'].';dbname='.$config['db_name'],$config['db_username'],$config['db_password']); // Setup RedBeanPHP
+      if ($config['db_freeze']) {
+        R::freeze();
+      }
+    }
+    return R::getToolBox(); // Returns RedBeanPHP's toolbox
+  }
+  
+}
+
+/**
 * View
-*
-* @desc    Used by Controller to load views.
+* 
+* 
+* Used by Controller to load views.
+* 
+* @package pipless
+* @subpackage system
+* 
 * @author  Gilbert Pellegrom, Dev7studios
-* @license MIT
-*
-* Copyright (c) 2011 Gilbert Pellegrom, Dev7studios
-* This source file is subject to the License that is bundled
-* with this source code in the file license.txt.
+* @author  Tom Andre Munkhaug, @tombayo <me@tombayo.com>
+* @license GPLv3
+* 
 */
 class View {
   
   /**
+   * Holds the variables to be exposed to the view.
+   * 
    * @var array
    */
   private $pageVars = array();
   
   /**
+   * The URL of the View's template.
+   * 
    * @var string
    */
   private $template;
 
   /**
-  * Constructor
-  * Creates a new instance of View by loading given template.
-  *
-  * @param string    $template   Name of template
-  */
-  public function __construct($template)  {
+   * Constructor
+   * Creates a new instance of View by loading given template.
+   *
+   * @param string $template Name of template
+   */
+  public function __construct(string $template)  {
     $this->template = APP_DIR .'views/'. $template .'.php';
   }
   
   /**
   * Sets variables to be forwarded to the template.
   *
-  * @param string    $var   Name of variable.
-  * @param mixed    $val   The variable's value.
-  *
-  * @return void
+  * @param string $var Name of variable.
+  * @param mixed  $val The variable's value.
   */
-  public function set($var, $val)  {
+  public function set(string $var, $val)  {
     $this->pageVars[$var] = $val;
   }
 
   /**
-  * Renders the View.
-  *
-  * @return void
-  */
-  public function render()  {
+   * Renders the view
+   * 
+   * If $ob_return is set to true, the function returns the output buffer.
+   * Otherwise echo the output buffer and return an empty string.
+   * 
+   * @param bool $ob_return
+   * @return string
+   */
+  public function render(bool $ob_return=false):string  {
     extract($this->pageVars);
 
     ob_start();
     require($this->template);
-    echo ob_get_clean();
+    if ($ob_return) {
+      return ob_get_clean();
+    } else {
+      echo ob_get_clean();
+      return '';
+    }
   }
 }
 
+/**
+ * Load
+ * 
+ *
+ * Used for loading controllers, views or plugins.
+ * 
+ * @package pipless
+ * @subpackage system
+ * 
+ * @author  Gilbert Pellegrom, Dev7studios
+ * @author  @tombayo <me@tombayo.com>
+ * @license GPLv3
+ *
+ */
 class Load {
+  /**
+   * Loads a given controller, and returns an instance of it.
+   * 
+   * @param string $name
+   * @return Controller
+   */
+  public static function controller(string $name):Controller {
+    require_once(APP_DIR . 'controllers/' . $name . '.php');
+    $controller = new $name;
+    return $controller;
+  }
   
   /**
   * Loads given View-class. Note: The name of the class must match filename.
   * Returns a new instance of given class.
   *
-  * @param string    $name   Name of class.
+  * @param string $name Name of class.
   *
-  * @return object
+  * @return View
   */
-  public static function view($name)  {
+  public static function view(string $name):View  {
     $view = new View($name);
     return $view;
   }
@@ -147,70 +226,122 @@ class Load {
   /**
   * Loads given Plugin by filename.
   *
-  * @param string    $name   Name of plugin/filename
-  *
-  * @return void
+  * @param string $name Name of plugin/filename
   */
-  public static function plugin($name)  {
-    require(APP_DIR .'plugins/'. strtolower($name) .'.php');
+  public static function plugin(string $name)  {
+    require_once(APP_DIR .'plugins/'. strtolower($name) .'.php');
   }
-  
-  /**
-  * Loads given Helper-class. Note: The name of the class must match filename.
-  * Returns a new instance of given class.
-  *
-  * @param string    $name   Name of helper.
-  *
-  * @return object
-  */
-  public static function helper($name)  {
-    require(APP_DIR .'helpers/'. strtolower($name) .'.php');
-    $helper = new $name;
-    return $helper;
-  }
-}
-
-/**
-* Interface for forcing subclasses to implement an index() method.
-*/
-interface Controller_demands {
-  public static function index();
 }
 
 /**
 * Parent class for Controllers.
-* Implements the Controller_demands interface to force error if the
-* controller don't have an index() method.
-* @abstract     To push the interface on the children of this class, and not on this class itself.
+* 
+* Declared abstract to be able to force methods to children.
+* 
+* @package pipless
+* @subpackage system
+* 
+* @author   Gilbert Pellegrom, Dev7studios
+* @author   Tom Andre Munkhaug, @tombayo <me@tombayo.com>
+* @license  GPLv3
+*  
 */
-abstract class Controller implements Controller_demands {
-    
+abstract class Controller {
+
+  /**
+   * Force the children of this class to implement a method index().
+   */
+  abstract public static function index();
+  
   /**
   * Redirects the client to a different url-suffix using base_url as prefix.
+  * 
+  * Also exits further code-execution.
   *
-  * @param string    $loc   Given url-suffix
-  *
-  * @return void
+  * @param string $loc Given url-suffix
   */
-  protected static function redirect($loc) {
+  protected static function redirect(string $loc) {
     header('Location: '. BASE_URL . $loc);
+    exit;
+  }
+}
+
+/**
+ * A trait used to define a basic controller.
+ * 
+ * A trait that loads the view "view.classname.php" and supplies the view with some basic variables.
+ * Used by controllers that only displays a standard index-view with no other logic.
+ * Should NOT be used by classes that don't extend Controller
+ * 
+ * @author Tom Andre Munkhaug <me@tombayo.com>
+ * @package pipless
+ * @subpackage traits 
+ */
+trait BasicWebpage {
+  
+  /**
+   * Loads the view "view.classname.php", supplies some basic variables, and renders the view.
+   */
+  public static function index() {
+    global $config;
+    $template = Load::view('view.'.__CLASS__);
+    $template->set('controller', __CLASS__);
+    $template->set('lang', $config['language']);
+    $template->render();
+  }
+}
+
+/**
+ * A trait for controllers supplying JSON-encoded data.
+ * 
+ * Calling setHeaders() will prepare the correct headers for the response.
+ * Also implements a basic index()-method which can be used for testing the controller from the front-end.
+ * 
+ * @author Tom Andre Munkhaug <me@tombayo.com>
+ * @package pipless
+ * @subpackage traits
+ * 
+ */
+trait JsonController {
+  
+  /**
+   * Sets headers for JSON-data.
+   */
+  private static function setHeaders() {
+    header("Content-Type: application/json; charset=UTF-8");
   }
   
   /**
-  * Initialize RedBeanPHP ORM for mysql.
-  * Then returns the Toolbox, for more details see:
-  * @link http://redbeanphp.com/api/class-RedBeanPHP.ToolBox.html
-  * 
-  * 
-  * @return RedBeanPHP/ToolBox
-  */
-  protected static function initDB() {
-    global $config;
-    if (!class_exists('R')) {
-      Load::plugin("rb"); // Loads RedBeanPHP, our ORM
-    }
-    R::setup('mysql:host='.$config['db_host'].';dbname='.$config['db_name'],$config['db_username'],$config['db_password']); // Setup RedBeanPHP
-    return R::getToolBox(); // Returns RedBeanPHP's toolbox
+   * Echoes "empty"=>"null" as JSON.
+   */
+  public static function index() {
+    self::setHeaders();
+    echo json_encode(['empty'=>'null']);
+  }  
+}
+
+/**
+ * A trait to enable calling static methods in an object context.
+ * 
+ * Used by controllers to enable them to be createable instances in order to
+ * use their private static functions within other controllers.
+ * 
+ * @author Tom Andre Munkhaug <me@tombayo.com>
+ * @package pipless
+ * @subpackage traits
+ *
+ */
+trait Objectify {
+  
+  /**
+   * Tries to call any unaccessible methods in this class.
+   * 
+   * @param string $name
+   * @param array $args
+   * 
+   * @return mixed
+   */
+  public function __call(string $name, array $args) {
+    return $this->$name(...$args);
   }
 }
-?>
